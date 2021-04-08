05c2efc4dfe9db530f3ea2ed5587fcf4dd56d941
Brannon Vann <brannon.vann@gmail.com>
2021-04-08 15:17:32 -0400

    file format only

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # kangaroo_x2_driver
+
 ROS driver for Kangaroo X2 motor driver board
 
-Takes a clean copy of the Kangaroo Arduino library provided by Dimension Engineering, and provides a reimplementation of Arduino.h in order to connect to the X2 through a linux serial port instead of Arduino serial. 
+Takes a clean copy of the Kangaroo Arduino library provided by Dimension Engineering, and provides a reimplementation of Arduino.h in order to connect to the X2 through a linux serial port instead of Arduino serial.
 
 The intention is to use this to control the X2 from a Raspberry Pi through an FTDI USB-serial driver.
 
@@ -9,27 +10,34 @@ Kangaroo X2 Arduino library:
 https://www.dimensionengineering.com/info/arduino
 
 ## Test the connection to the motor driver:
+
 ```
 rosrun kangaroo_x2_driver SpeedControlExample
 ```
+
 (this will run motor 1 back and forward)
 
 ## Create separate controllers for motors 1 and 2:
+
 ```
 roslaunch kangaroo_x2_driver independent_speed_control.launch
 ```
-The controllers listen for speed commands of type std_msgs/Float64 on topics: 
+
+The controllers listen for speed commands of type std_msgs/Float64 on topics:
+
 ```
 /motor_1_controller/command
 /motor_2_controller/command
 ```
 
 ## Differential drive control:
+
 ```
 roslaunch kangaroo_x2_driver diff_drive_control.launch
 ```
+
 The differential-drive controller will listen for twist velocity commands published to topic `/diff_drive_controller/cmd_vel`.
 
-
 ## Kangaroo Setup
+
 Ensure the kangaroo board is configured for independent mode (dip-switch 4 is ON) and digital input (dip-switch 1 is ON). Also ensure the board has been tuned with these settings.
--- src/kangaroo_x2_driver_node.cpp
+++ src/kangaroo_x2_driver_node.cpp
@@ -9,164 +9,179 @@
 
 using namespace std;
 
-
-inline const char* toString(KangarooError err)
+inline const char *toString(KangarooError err)
 {
     switch (err)
     {
-        case KANGAROO_NO_ERROR:         return "KANGAROO_NO_ERROR";
-        case KANGAROO_NOT_STARTED:      return "KANGAROO_NOT_STARTED";
-        case KANGAROO_NOT_HOMED:        return "KANGAROO_NOT_HOMED";
-        case KANGAROO_CONTROL_ERROR:    return "KANGAROO_CONTROL_ERROR";
-        case KANGAROO_WRONG_MODE:       return "KANGAROO_WRONG_MODE";
-        case KANGAROO_SERIAL_TIMEOUT:   return "KANGAROO_SERIAL_TIMEOUT";
-        case KANGAROO_TIMED_OUT:        return "KANGAROO_TIMED_OUT";
-        default:                        return "Unknown error";
+    case KANGAROO_NO_ERROR:
+        return "KANGAROO_NO_ERROR";
+    case KANGAROO_NOT_STARTED:
+        return "KANGAROO_NOT_STARTED";
+    case KANGAROO_NOT_HOMED:
+        return "KANGAROO_NOT_HOMED";
+    case KANGAROO_CONTROL_ERROR:
+        return "KANGAROO_CONTROL_ERROR";
+    case KANGAROO_WRONG_MODE:
+        return "KANGAROO_WRONG_MODE";
+    case KANGAROO_SERIAL_TIMEOUT:
+        return "KANGAROO_SERIAL_TIMEOUT";
+    case KANGAROO_TIMED_OUT:
+        return "KANGAROO_TIMED_OUT";
+    default:
+        return "Unknown error";
     }
 }
 
 class KangarooX2 : public hardware_interface::RobotHW
 {
 public:
-  KangarooX2(string port, unsigned long baud, int ticksPerWheelRev) : 
-    serial_port(port, baud, serial::Timeout::simpleTimeout(1000)),
-    stream(serial_port),
-    K(stream),
-    K1(K, '1'),
-    K2(K, '2') {
+    KangarooX2(string port, unsigned long baud, int ticksPerWheelRev) : serial_port(port, baud, serial::Timeout::simpleTimeout(1000)),
+                                                                        stream(serial_port),
+                                                                        K(stream),
+                                                                        K1(K, '1'),
+                                                                        K2(K, '2')
+    {
 
-    startChannels();
- 
-    //Ticks to radians conversion: determined by rotating wheel 10 timees, 
-    //then calling 1,getp over simple serial
-    //TODO: implement scaling through KangarooChannel::units call
-    //  also implement optional reversal of motors
-    ticksToRadians = 2*M_PI / ticksPerWheelRev; 
-    radiansToTicks = 1/ticksToRadians;
-    
-    pos_[0] = 0.0; pos_[1] = 0.0;
-    vel_[0] = 0.0; vel_[1] = 0.0;
-    eff_[0] = 0.0; eff_[1] = 0.0;
-    cmd_[0] = 0.0; cmd_[1] = 0.0;
+        startChannels();
 
-    hardware_interface::JointStateHandle state_handle_1("motor_1_joint", &pos_[0], &vel_[0], &eff_[0]);
-    jnt_state_interface_.registerHandle(state_handle_1);
+        //Ticks to radians conversion: determined by rotating wheel 10 timees,
+        //then calling 1,getp over simple serial
+        //TODO: implement scaling through KangarooChannel::units call
+        //  also implement optional reversal of motors
+        ticksToRadians = 2 * M_PI / ticksPerWheelRev;
+        radiansToTicks = 1 / ticksToRadians;
 
-    hardware_interface::JointStateHandle state_handle_2("motor_2_joint", &pos_[1], &vel_[1], &eff_[1]);
-    jnt_state_interface_.registerHandle(state_handle_2);
+        pos_[0] = 0.0;
+        pos_[1] = 0.0;
+        vel_[0] = 0.0;
+        vel_[1] = 0.0;
+        eff_[0] = 0.0;
+        eff_[1] = 0.0;
+        cmd_[0] = 0.0;
+        cmd_[1] = 0.0;
 
-    registerInterface(&jnt_state_interface_);
+        hardware_interface::JointStateHandle state_handle_1("motor_1_joint", &pos_[0], &vel_[0], &eff_[0]);
+        jnt_state_interface_.registerHandle(state_handle_1);
 
-    hardware_interface::JointHandle vel_handle_1(jnt_state_interface_.getHandle("motor_1_joint"), &cmd_[0]);
-    jnt_vel_interface_.registerHandle(vel_handle_1);
+        hardware_interface::JointStateHandle state_handle_2("motor_2_joint", &pos_[1], &vel_[1], &eff_[1]);
+        jnt_state_interface_.registerHandle(state_handle_2);
 
-    hardware_interface::JointHandle vel_handle_2(jnt_state_interface_.getHandle("motor_2_joint"), &cmd_[1]);
-    jnt_vel_interface_.registerHandle(vel_handle_2);
+        registerInterface(&jnt_state_interface_);
 
-    registerInterface(&jnt_vel_interface_);
-  }
+        hardware_interface::JointHandle vel_handle_1(jnt_state_interface_.getHandle("motor_1_joint"), &cmd_[0]);
+        jnt_vel_interface_.registerHandle(vel_handle_1);
 
-  ~KangarooX2(){
-    K1.s(0);
-    K2.s(0);
-  }
+        hardware_interface::JointHandle vel_handle_2(jnt_state_interface_.getHandle("motor_2_joint"), &cmd_[1]);
+        jnt_vel_interface_.registerHandle(vel_handle_2);
 
-  ros::Time getTime() const {return ros::Time::now();}
-  ros::Duration getPeriod() const {return ros::Duration(0.01);}
-
-  void read(){
-    ROS_INFO_STREAM("Commands for joints: " << cmd_[0] << ", " << cmd_[1]);
-    //send commands to motors
-    K1.s((int32_t)  cmd_[0] * radiansToTicks);
-    K2.s((int32_t) -cmd_[1] * radiansToTicks);
-  }
-
-  void write(){
-    KangarooStatus resultGetK1P = K1.getP();
-    KangarooStatus resultGetK2P = K2.getP();
-
-    bool allOk = resultGetK1P.ok() && resultGetK2P.ok();
-    if (!allOk){
-        //assume not ok because channels need to be restarted
-        //this happens when kangaroo board is reset
-        //report error codes, wait a bit, then
-        //try restarting channels
-        ROS_ERROR_STREAM(
-            "Kangaroo status not OK" << endl <<
-            "Motor 1 state: " << toString(resultGetK1P.error()) << endl <<
-            "Motor 2 state: " << toString(resultGetK2P.error())
-        );
-        
-        //warning: before adding the sleep statement below, I burnt out
-        //a motor, possbily because of the instant restart on error conditions
-        //(
-        ros::Duration(10).sleep();        
-        startChannels(); 
+        registerInterface(&jnt_vel_interface_);
     }
-    else { 
-        pos_[0] =  resultGetK1P.value() * ticksToRadians;
-        pos_[1] = -resultGetK2P.value() * ticksToRadians;
-        vel_[0] =  K1.getS().value() * ticksToRadians;
-        vel_[1] = -K2.getS().value() * ticksToRadians;
+
+    ~KangarooX2()
+    {
+        K1.s(0);
+        K2.s(0);
     }
-  }
+
+    ros::Time getTime() const { return ros::Time::now(); }
+    ros::Duration getPeriod() const { return ros::Duration(0.01); }
+
+    void read()
+    {
+        ROS_INFO_STREAM("Commands for joints: " << cmd_[0] << ", " << cmd_[1]);
+        //send commands to motors
+        K1.s((int32_t)cmd_[0] * radiansToTicks);
+        K2.s((int32_t)-cmd_[1] * radiansToTicks);
+    }
+
+    void write()
+    {
+        KangarooStatus resultGetK1P = K1.getP();
+        KangarooStatus resultGetK2P = K2.getP();
+
+        bool allOk = resultGetK1P.ok() && resultGetK2P.ok();
+        if (!allOk)
+        {
+            //assume not ok because channels need to be restarted
+            //this happens when kangaroo board is reset
+            //report error codes, wait a bit, then
+            //try restarting channels
+            ROS_ERROR_STREAM(
+                "Kangaroo status not OK" << endl
+                                         << "Motor 1 state: " << toString(resultGetK1P.error()) << endl
+                                         << "Motor 2 state: " << toString(resultGetK2P.error()));
+
+            //warning: before adding the sleep statement below, I burnt out
+            //a motor, possbily because of the instant restart on error conditions
+            //(
+            ros::Duration(10).sleep();
+            startChannels();
+        }
+        else
+        {
+            pos_[0] = resultGetK1P.value() * ticksToRadians;
+            pos_[1] = -resultGetK2P.value() * ticksToRadians;
+            vel_[0] = K1.getS().value() * ticksToRadians;
+            vel_[1] = -K2.getS().value() * ticksToRadians;
+        }
+    }
 
 private:
-  hardware_interface::JointStateInterface    jnt_state_interface_;
-  hardware_interface::VelocityJointInterface jnt_vel_interface_;
-  double ticksToRadians;
-  double radiansToTicks;
-  double cmd_[2];
-  double pos_[2];
-  double vel_[2];
-  double eff_[2];
+    hardware_interface::JointStateInterface jnt_state_interface_;
+    hardware_interface::VelocityJointInterface jnt_vel_interface_;
+    double ticksToRadians;
+    double radiansToTicks;
+    double cmd_[2];
+    double pos_[2];
+    double vel_[2];
+    double eff_[2];
 
-  serial::Serial serial_port;
-  SerialStream stream;
-  KangarooSerial  K;
-  KangarooChannel K1;
-  KangarooChannel K2;
+    serial::Serial serial_port;
+    SerialStream stream;
+    KangarooSerial K;
+    KangarooChannel K1;
+    KangarooChannel K2;
 
-//set up kangaroo channels
-void startChannels() {
-  ROS_INFO_STREAM("Starting Kangaroo channels");
-  K1.start();
-  K2.start();
-}
-
+    //set up kangaroo channels
+    void startChannels()
+    {
+        ROS_INFO_STREAM("Starting Kangaroo channels");
+        K1.start();
+        K2.start();
+    }
 };
 
 int main(int argc, char **argv)
 {
-  double x, y, theta;
-  
-  ros::init(argc, argv, "kangaroo_x2");
-  ros::NodeHandle nh;
- 
-  string port;
-  nh.getParam("/serialPort", port);
-  double baud;
-  nh.getParam("/baudRate", baud);
-  int ticksPerWheelRev;
-  nh.getParam("/ticksPerWheelRev", ticksPerWheelRev);
+    double x, y, theta;
 
-  KangarooX2 robot(port, baud, ticksPerWheelRev );
-  ROS_INFO_STREAM("period: " << robot.getPeriod().toSec());
-  controller_manager::ControllerManager cm(&robot, nh);
+    ros::init(argc, argv, "kangaroo_x2");
+    ros::NodeHandle nh;
 
-  ros::Rate rate(1.0 / robot.getPeriod().toSec());
-  ros::AsyncSpinner spinner(1);
-  spinner.start();
+    string port;
+    nh.getParam("/serialPort", port);
+    double baud;
+    nh.getParam("/baudRate", baud);
+    int ticksPerWheelRev;
+    nh.getParam("/ticksPerWheelRev", ticksPerWheelRev);
 
-  while(ros::ok())
-  {
-    //TODO: Detect loss of connection to robot and reconnect
-    robot.read();
-    robot.write();
-    cm.update(robot.getTime(), robot.getPeriod());
-    rate.sleep();
-  }
-  spinner.stop();
+    KangarooX2 robot(port, baud, ticksPerWheelRev);
+    ROS_INFO_STREAM("period: " << robot.getPeriod().toSec());
+    controller_manager::ControllerManager cm(&robot, nh);
 
-  return 0;
+    ros::Rate rate(1.0 / robot.getPeriod().toSec());
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
+
+    while (ros::ok())
+    {
+        //TODO: Detect loss of connection to robot and reconnect
+        robot.read();
+        robot.write();
+        cm.update(robot.getTime(), robot.getPeriod());
+        rate.sleep();
+    }
+    spinner.stop();
+
+    return 0;
 }
